cd89675fd41b0b8bc5385c32e83084c0deb3792f
nnorukaemeka <nnorukaemeka@gmail.com>
2019-11-17 06:47:19 +0100

    Employee pages added and connected

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/nuru/my_flask_app/venv"
+set -gx VIRTUAL_ENV "/home/megzy/PycharmProjects/codeProjects/flask_framework/cp_news/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
